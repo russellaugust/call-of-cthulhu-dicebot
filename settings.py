53e939efb6dcd6a7e1f44365b40f7bbc9b7d93d6
Russell Anderson <plex@server.plexbox.private>
2020-10-20 14:37:47 -0700

    includes property decorators

--- settings.py
+++ settings.py
@@ -2,22 +2,50 @@
 
 import yaml
 
-# with open('settings.yaml') as f:
-    
-#     list_doc = yaml.load(f, Loader=yaml.FullLoader)
-#     #print(data['doe'])
+SETTINGSFILE = "settings.yaml"
 
-#     # for sense in list_doc:
-#     #     if sense["name"] == "sense2":
-#     #         sense["value"] = 1234
+class Settings:
 
-#     #with open("my_file.yaml", "w") as f:
-#     yaml.dump(list_doc, "test.yaml")
+    def __init__(self):
+        self.__settings__ = {}
+        with open(SETTINGSFILE) as f:
+            self.__settings__ = yaml.load(f, Loader=yaml.FullLoader)
 
-data = {"announce":False,
-        "card location":"/mnt/path",
-        "database location":"/mnt/database/path/name.db"
-}
+    def __update__(self, field, info):
+        with open(SETTINGSFILE, 'w') as outfile:
+            self.__settings__[field] = info
+            yaml.dump(self.__settings__, outfile, default_flow_style=False)
+            
+        
+    @property
+    def announce(self):
+        return self.__settings__['announce']
 
-with open('settings.yaml', 'w') as outfile:
-    yaml.dump(data, outfile, default_flow_style=False)
+    @announce.setter
+    def announce(self, announce_bool):
+        if isinstance(announce_bool, bool):
+            self.__update__("announce", announce_bool)
+        else:
+            raise ValueError('This needs to be True or False')
+
+    @property
+    def card_path(self):
+        return self.__settings__['card_path']
+
+    @card_path.setter
+    def card_path(self, input):
+        self.__update__("card_path", input)
+
+
+    @property
+    def database_path(self):
+        return self.__settings__['database_path']
+
+    @database_path.setter
+    def database_path(self, input):
+        self.__update__("database_path", input)
+
+
+    @property
+    def audio_rolls_path(self):
+        return self.__settings__['audio_rolls_path']
