8a7cd89dec61364bc9212521e6e1cd7200e68f49
Russell Anderson <russell.august@gmail.com>
2021-05-02 01:09:53 -0700

    added gifs

--- settings.py
+++ settings.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import yaml
+import requests
+import validators
 
 SETTINGSFILE = "settings.yaml"
 
@@ -66,7 +68,47 @@ class Settings:
         self.__update__("voice_volume", input)
 
 
+    @property
+    def gif_critical(self):
+        self.__refresh__()
+        return self.__settings__['gif_critical']
+
+    @gif_critical.setter
+    def gif_critical(self, input):
+        self.__update__("gif_critical", input)
+
+    @property
+    def gif_fumble(self):
+        self.__refresh__()
+        return self.__settings__['gif_fumble']
+
+    @gif_fumble.setter
+    def gif_fumble(self, input):
+        self.__update__("gif_fumble", input)
+
+    @property
+    def gif_lucky(self):
+        self.__refresh__()
+        return self.__settings__['gif_lucky']
+
+    @gif_lucky.setter
+    def gif_lucky(self, input):
+        self.__update__("gif_lucky", input)
+
+
+    def link_exists(self, link):
+        if validators.url(link):
+            request = requests.get(link)
+            if request.status_code == 200:
+                print('Web site exists')
+            else:
+                print('Web site does not exist') 
 
 if __name__ == '__main__':
     x = Settings()
     print (x.voice_volume)
+    print (x.gif_critical)
+    print (x.gif_fumble)
+    print (x.gif_lucky)
+
+    x.link_exists(x.gif_lucky)
