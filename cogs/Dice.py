4de50931c57f69975a9934123036d62965bdbd3e
Russell Anderson <russell.august@gmail.com>
2021-05-31 23:03:18 -0700

    major consolidation of rolls into single function

--- cogs/Dice.py
+++ cogs/Dice.py
@@ -13,7 +13,65 @@ class Dice(commands.Cog):
         self.settings = Settings()
         self.db = Database(self.settings.database_path)
 
-        print ('test')
+    def __roll_with_format__(self, ctx, rolls, additional_comment=""):
+
+        # FAIL: the sum of the rolls is NONE, which indicates there was a problem in the syntax or code.  Produces error.
+        if rolls.getroll().error():
+            embed = discord.Embed(colour=discord.Colour(0xbf1919), description="*SPROÜTS!*   That's some bad syntax.")
+
+        # FAIL: if someone accidentally writes d00, which means roll a zero-sided die
+        elif "d00" in rolls.getroll().get_argument():
+            embed = discord.Embed(colour=discord.Colour(0xbf1919), description="This is embarrassing... I doubt you meant to roll that.")
+
+        # PASS: If the success field in the first dice roll is filled in, then that means it had to be a stat roll with a 1d100 so it. 
+        elif rolls.getroll().stat_exists():
+            pass
+
+        description = ""
+        color = 0x0968ed
+
+        for roll in rolls.getrolls():
+            description += roll.get_string() + "\n"
+            color = color if roll.is_omitted() else roll.get_success_color()
+
+        comment = rolls.getroll().get_comment() if rolls.getroll().get_comment() else ""
+        embed = discord.Embed(title=f"{comment}{additional_comment}", colour=discord.Colour(color), description=description)
+
+        author_avatar_url = ctx.author.avatar_url or ctx.author.default_avatar_url
+        embed.set_author(name=ctx.author.display_name, icon_url=author_avatar_url)
+
+        return embed
+
+    def __successlevel_image__(self, embed, roll):
+        # returns a discord.embed image with a gif that matches the roll success
+        #if roll is not diceroller.RollResult:
+        #    raise ValueError('not class RollResult.')
+        if roll.get_success() == "lucky success":
+            embed.set_image(url=self.settings.gif_random_lucky)
+        elif roll.get_success() == "critical":
+            embed.set_image(url=self.settings.gif_random_critical)
+        elif roll.get_success() == "fumble":
+            embed.set_image(url=self.settings.gif_random_fumble)
+        return embed
+
+    def __announce_roll__(self, ctx, rolls):
+        vc = ctx.voice_client
+        if self.settings.announce and vc and rolls.getroll().stat_exists():
+            success = rolls.getroll().get_success() if rolls.getroll().get_success() is not None else ""
+            success_slugify = slugify(success, lowercase=True)
+
+            voicefolder = "audio/diceroll-voice/eldritch"
+            max_vo_variations = self.count_files_starting_with(voicefolder, success_slugify)
+
+            path = f'{voicefolder}/{success_slugify}-{random.randint(1,max_vo_variations)}.mp3'
+            self.song_queue.append(path)
+
+            if not vc.is_playing():
+                self.song_queue.insert(0, "audio/diceroll1.mp3")
+                self.song_queue.insert(len(self.song_queue)-1, "audio/DramaticSting1.mp3") if success == "critical" or success == "fumble" else None
+                
+                # Play audio in channel
+                self.play_next(ctx)
         
     @commands.command(  pass_context=True, 
                         name='roll',
@@ -22,15 +80,10 @@ class Dice(commands.Cog):
                         description="The ROLL command works by selecting the dice you want (1D100) and modifiers (+1) an optional stat (45) and a comment (# pump some lead into that goon!)"
                         )
     async def rollcommand(self, ctx, *, arg=None):
-        '''main roll command. this will allow the user to roll dice assuming some basic syntax is used.'''
 
-        # if the user only types the command !r, it roll a 1d100
-        arg = arg if arg else "1D100"
-        
-        # rolls the dice and gets stores the result as a list
-        dice = diceroller.DiceRolls(arg)
-        print (dice)
-        description = ""
+        # rolls the dice and stores the result as a list. if no argument, roll the default
+        dice = diceroller.DiceRolls(arg if arg else self.settings.dice_default)
+        embed = self.__roll_with_format__(ctx=ctx, rolls=dice)
 
         # ADD TO DB: putting all results in the database.  It skips when there's nothing, including failures and syntax errors!
         for roll in dice.getrolls():
@@ -42,72 +95,12 @@ class Dice(commands.Cog):
                 self.db.add_roll(str(ctx.author), ctx.author.display_name, arg, roll.get_equation(), roll.get_sumtotal(), roll.get_stat(), roll.get_success(), roll.get_comment(), str(ctx.guild), str(ctx.channel))
                 print("{} {} {} {} {} {} {} {} {} {}".format(ctx.guild, ctx.channel, ctx.author, ctx.author.display_name, arg, roll.get_equation(), roll.get_sumtotal(), roll.get_stat(), roll.get_success(), roll.get_comment()))
 
-        # FAIL: in this event, the sum of the rolls is NONE, which indicates there was a problem in the syntax or code.  Produces error.
-        if dice.getroll().get_sumtotal() == None:
-            embed = discord.Embed(colour=discord.Colour(0xbf1919), description="*SPROÜTS!*   That's some bad syntax.")
-
-        # FAIL: condition if someone accidentally writes d00, which means roll a zero-sided die
-        elif "d00" in dice.getroll().get_argument():
-            embed = discord.Embed(colour=discord.Colour(0xbf1919), description="This is embarrassing... Check that roll and try again.")
-
-        # PASS: If the success field in the first dice roll is filled in, then that means it had to be a stat roll with a 1d100 so it. 
-        elif dice.getroll().stat_exists():
-            for roll in dice.getrolls():
-                description += "{} is a ***{}***\n".format(roll.get_sumtotal(), roll.get_success())
+        # embed a GIF or image for these special situations
+        embed = self.__successlevel_image__(embed, dice.getroll())
             
-            # sets the color to the level of success. if there's more than 1 roll, then use a special color
-            colour = dice.getroll().get_success_color() if dice.get_roll_count() == 1 else 0x0968ed
-                    
-            embed = discord.Embed(title=roll.get_comment() if roll.get_comment() else "", colour=discord.Colour(colour), description=description)
-            
-            # embed a GIF or image for these special situations
-            if dice.getroll().get_success() == "lucky success":
-                embed.set_image(url=self.settings.gif_lucky)
-            elif dice.getroll().get_success() == "critical":
-                embed.set_image(url=self.settings.gif_critical)
-            elif dice.getroll().get_success() == "fumble":
-                embed.set_image(url=self.settings.gif_fumble)
-
-            # read the results if announce is on and the bot is in a channel
-            vc = ctx.voice_client
-            if self.settings.announce and vc:
-                success = dice.getroll().get_success() if dice.getroll().get_success() is not None else ""
-                success_slugify = slugify(success, lowercase=True)
-
-                voicefolder = "audio/diceroll-voice/eldritch"
-                max_vo_variations = self.count_files_starting_with(voicefolder, success_slugify)
-
-                path = f'{voicefolder}/{success_slugify}-{random.randint(1,max_vo_variations)}.mp3'
-                self.song_queue.append(path)
-
-                try:
-                    print(self.song_queue)
-                    if not vc.is_playing():
-                        self.song_queue.insert(0, "audio/diceroll1.mp3")
-                        self.song_queue.insert(len(self.song_queue)-1, "audio/DramaticSting1.mp3") if success == "critical" or success == "fumble" else None
-                        
-                        # Play audio in channel
-                        self.play_next(ctx)
-
-                # Handle the exceptions that can occur, i don't entirely understand this though, so I commented it out
-                except discord.ClientException as e:
-                    await ctx.send(f"A client exception occured:\n`{e}`")
-                except TypeError as e:
-                    await ctx.send(f"TypeError exception:\n`{e}`")
-                except discord.opus.OpusNotLoaded as e:
-                    await ctx.send(f"OpusNotLoaded exception: \n`{e}`")            
+        # read the results if announce is on and the bot is in a channel
+        self.__announce_roll__(ctx, dice)          
         
-        # PASS: this is every other roll condition. 
-        else:
-            for roll in dice.getrolls():
-                description += f"{roll.get_string()}\n"
-
-            embed = discord.Embed(
-                title=roll.get_comment() if roll.get_comment() else "",
-                colour=discord.Colour(0x24ed60), 
-                description=description
-                )
-
         # dramatic pause for fumbles and criticals  
         await asyncio.sleep(4) if dice.getroll().get_success() == "fumble" or dice.getroll().get_success() == "critical" else None
         
@@ -115,59 +108,31 @@ class Dice(commands.Cog):
         embed.set_author(name=ctx.author.display_name, icon_url=author_avatar_url)
         await ctx.send(embed=embed)
 
+    @commands.command(pass_context=True)
+    async def testroll(self, ctx, *, arg=None):
+        dice = diceroller.DiceRolls(arg if arg else self.settings.dice_default, repeat=1, omit_state=0)
+        dice.override_sumtotal(int(arg))
+        embed = self.__roll_with_format__(ctx=ctx, rolls=dice, additional_comment=" - testing only.")
+        embed = self.__successlevel_image__(embed, dice.getroll())
+        await ctx.send(embed=embed)
+
     @commands.command(pass_context=True, aliases=['r+', 'b'])
     async def bonus(self, ctx, *, arg=None):
-
-        # if the user only types the command !r, it roll a 1d100
-        arg = arg if arg else "1D100"
-        
-        dice1 = diceroller.DiceRolls(arg).getroll()
-        dice2 = diceroller.DiceRolls(arg).getroll()
-
-        description = f"{dice1.get_string()}\n~~{dice2.get_string()}~~" if dice1.get_sumtotal() <= dice2.get_sumtotal() else f"~~{dice1.get_string()}~~\n{dice2.get_string()}"
-        color = dice1.get_success_color() if dice1.get_sumtotal() <= dice2.get_sumtotal() else dice2.get_success_color()
-
-        embed = discord.Embed(title=dice1.get_comment(), colour=color, description=description)
-        author_avatar_url = ctx.author.avatar_url or ctx.author.default_avatar_url
-        embed.set_author(name=ctx.author.display_name, icon_url=author_avatar_url)
-
-        comment = dice1.get_comment() if dice1.get_comment() else ""
-        embed = discord.Embed(
-            title=f"{comment} - with bonus.",
-            colour=discord.Colour(0x24ed60), 
-            description=description
-            )
-
+        dice = diceroller.DiceRolls(arg if arg else self.settings.dice_default, repeat=2, omit_state=1)
+        embed = self.__roll_with_format__(ctx=ctx, rolls=dice, additional_comment=" - with bonus.")
         await ctx.send(embed=embed)
 
     @commands.command(pass_context=True, aliases=['r-', 'p'])
     async def penalty(self, ctx, *, arg=None):
-
-        # if the user only types the command !r, it roll a 1d100
-        arg = arg if arg else "1D100"
-        
-        dice1 = diceroller.DiceRolls(arg).getroll()
-        dice2 = diceroller.DiceRolls(arg).getroll()
-
-        description = f"{dice1.get_string()}\n~~{dice2.get_string()}~~" if dice1.get_sumtotal() >= dice2.get_sumtotal() else f"~~{dice1.get_string()}~~\n{dice2.get_string()}"
-        color = dice1.get_success_color() if dice1.get_sumtotal() >= dice2.get_sumtotal() else dice2.get_success_color()
-
-        embed = discord.Embed(title=dice1.get_comment(), colour=color, description=description)
-        author_avatar_url = ctx.author.avatar_url or ctx.author.default_avatar_url
-        embed.set_author(name=ctx.author.display_name, icon_url=author_avatar_url)
-
-        comment = dice1.get_comment() if dice1.get_comment() else ""
-        embed = discord.Embed(
-            title=f"{comment} - with penalty.",
-            colour=discord.Colour(0x24ed60), 
-            description=description
-            )
-
+        dice = diceroller.DiceRolls(arg if arg else self.settings.dice_default, repeat=2, omit_state=-1)
+        embed = self.__roll_with_format__(ctx=ctx, rolls=dice, additional_comment=" - with penalty.")
         await ctx.send(embed=embed)
 
     @commands.command(pass_context=True, aliases=['rp', 'rpt'])
     async def repeat(self, ctx, *, arg=None):
-        await ctx.send("not yet implemented.")
+        dice = diceroller.DiceRolls(arg if arg else self.settings.dice_default, repeat=10, omit_state=0)
+        embed = self.__roll_with_format__(ctx=ctx, rolls=dice, additional_comment=" - repeat.")
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=['i'], 
                       brief='-i [STATS TO IMPROVE] e.g. -i 50, or -i 30 50 40 30 for multiple stats.',  
@@ -192,6 +157,26 @@ class Dice(commands.Cog):
         embed = discord.Embed(colour=discord.Colour(0x24ed60), description=description)
         embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.default_avatar_url)    
         await ctx.send(embed=embed)
+
+    @commands.command(pass_context=True)
+    async def lastrolls(self, ctx, *, arg=None):
+
+        if mathtools.RepresentsInt(arg): #is the argument an integer value
+            rolls = self.db.get_entries_as_string(number_of_entries=int(arg))
+            description = "LAST ROLLS\n"
+            for roll in rolls:
+                print (roll)
+                description += f"{roll}\n"
+
+            embed = discord.Embed(
+                colour=discord.Colour(0x24ed60), 
+                description=description
+                )
+
+            await ctx.send(embed=embed)
+        
+        else:
+            await ctx.send("bad syntax.")
 
     def play_next(self, ctx):
         if len(self.song_queue) >= 1:
