9e24d3a5d986cb9a8ef02df0523a7f742212c14b
Russell Anderson <russell.august@gmail.com>
2022-07-11 16:43:05 -0700

    strip trash in code fences

--- discord-bot/mdtools.py
+++ discord-bot/mdtools.py
@@ -9,5 +9,8 @@ def extract_code_fences(text):
     for matchNum, match in enumerate(matches, start=1):
         for groupNum in range(0, len(match.groups())):
             groupNum = groupNum + 1
-            code_fences.append(match.group(groupNum))
+            code_fences.append(match.group(groupNum).strip())
     return code_fences
+
+# if __name__ == "__main__":
+#    Do something here
