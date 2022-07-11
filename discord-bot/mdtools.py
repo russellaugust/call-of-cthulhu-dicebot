cadf927a6a3059f5d0cc7e80cf7014e7b0399cdb
Russell Anderson <russell.august@gmail.com>
2022-07-11 16:44:42 -0700

    revert strip

--- discord-bot/mdtools.py
+++ discord-bot/mdtools.py
@@ -9,7 +9,7 @@ def extract_code_fences(text):
     for matchNum, match in enumerate(matches, start=1):
         for groupNum in range(0, len(match.groups())):
             groupNum = groupNum + 1
-            code_fences.append(match.group(groupNum).strip())
+            code_fences.append(match.group(groupNum))
     return code_fences
 
 # if __name__ == "__main__":
