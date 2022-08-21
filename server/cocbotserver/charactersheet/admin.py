49fbebdcfe5e4ff370792b6dbac210761bfd2e40
Russell Anderson <russell.august@gmail.com>
2022-08-20 23:23:35 -0700

    added characterskill fixes points add correctly added weapons fixed relationships

--- server/cocbotserver/charactersheet/admin.py
+++ server/cocbotserver/charactersheet/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Location, Character, Skill, Weapon, Player, CharacterSkill
+from .models import Location, Character, Skill, Weapon, Player, CharacterSkill, CharacterWeapon
 from .models import Roll, DiscordMessage, DiscordChannel
 
 # Register your models here.
@@ -10,5 +10,6 @@ admin.site.register(Character)
 admin.site.register(Skill)
 admin.site.register(Weapon)
 admin.site.register(CharacterSkill)
+admin.site.register(CharacterWeapon)
 admin.site.register(Location)
 admin.site.register(DiscordChannel)
